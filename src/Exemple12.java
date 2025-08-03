6112ee1b64f3602d6cb8f2663f6ce3fea6f10aeb
tawbazf <tawbazehaf@gmail.com>
2025-08-03 23:56:24 +0100

    string methods

---
+++ src/Exemple12.java
@@ -0,0 +1,25 @@
+public class Exemple12 {
+    //string methods
+    public static void main(String[] args) {
+        String str = "Hello, World!";
+        
+        //length
+        System.out.println("Length: " + str.length());
+        
+        //charAt
+        System.out.println("Character at index 7: " + str.charAt(7));
+        
+        //substring
+        System.out.println("Substring from index 7: " + str.substring(7));
+        
+        //indexOf
+        System.out.println("Index of 'World': " + str.indexOf("World"));
+        
+        //toLowerCase and toUpperCase
+        System.out.println("Lowercase: " + str.toLowerCase());
+        System.out.println("Uppercase: " + str.toUpperCase());
+        
+        //replace
+        System.out.println("Replace 'World' with 'Java': " + str.replace("World", "Java"));
+    }
+}
